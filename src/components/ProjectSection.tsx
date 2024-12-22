454ed2694b6d9028d614aecd61748c559de957d2
Akshay Patel <akshaypatel.ap1320@gmail.com>
2024-12-22 11:02:33 +0530

    Project section minor modification in gap

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -15,25 +15,27 @@ const ProjectSection = () => {
         </svg>
         <span className="text-2xl text-secondary">Projects</span>
       </div>
-      {projects.map((project, index) => {
-        return (
-          <div key={index} className="flex justify-between gap-10">
-            <div>
-              <div className="text-xl font-medium">{project.name}</div>
-              <div className="text-lg font-normal text-secondary">
-                {project.description}
+      <div className="flex flex-col gap-10">
+        {projects.map((project, index) => {
+          return (
+            <div key={index} className="flex justify-between gap-10">
+              <div>
+                <div className="text-xl font-medium">{project.name}</div>
+                <div className="text-base font-normal text-secondary">
+                  {project.description}
+                </div>
+              </div>
+              <div className="flex-shrink-0">
+                <img
+                  src={`${project.image}`}
+                  alt="Rawg clone image"
+                  className="h-auto w-56 rounded-lg border-2 border-gray-600 object-cover shadow-md"
+                ></img>
               </div>
             </div>
-            <div className="flex-shrink-0">
-              <img
-                src={`${project.image}`}
-                alt="Rawg clone image"
-                className="h-auto w-56 rounded-lg border-2 border-gray-600 object-cover shadow-md"
-              ></img>
-            </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 };
